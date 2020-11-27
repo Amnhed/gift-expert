3aeadf436d4931ee3baf41aba3c654f10ce2b379
Amnhed <aalhe0.0@gmail.com>
2020-11-27 02:04:49 -0600

    Modificando el arreglo con un evento en el boton
        const handleAdd = () => {
            setCategorias([...categorias,'HunterXhunter'])
            console.log(categorias);
        }
    button onClick={handleAdd} >

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifExpertApp = () => {
+    const [categorias, setCategorias] = useState(['Dragon Ball', 'One punch']);
+
+    const handleAdd = () => {
+        setCategorias([...categorias,'HunterXhunter']) 
+        console.log(categorias);
+    }
+
     return (
         <div>
             <h2>Gift expert APP</h2>
             <hr/>
+            <button onClick={handleAdd} >Agregar</button>
+            <ol>
+                {
+                    categorias.map( category => {
+                    return <li key={category}>{ category }</li>
+                    })
+                }
+            </ol>
+
         </div>
     )
 }
